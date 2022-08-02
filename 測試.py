c28400d896f6e04b4b5a63d815634096f6fb0fb2
MaMaverick86 <110382124+MaMaverick86@users.noreply.github.com>
2022-08-02 11:25:12 +0800

    Add files via upload

---
+++ 測試.py
@@ -0,0 +1,19 @@
+
+import tkinter as tk
+import tkinter.messagebox as tkMessageBox
+
+win = tk.Tk()
+win.geometry("400x400")
+win.configure(bg='#303841')
+
+tkMessageBox.showinfo("Entry","Missing file.")
+tkMessageBox.showwarning("ERROR","Warning!!!")
+tkMessageBox.showerror("Auto","Check your file.")
+result=tkMessageBox.askquestion("Recover","Download?")
+print(result)
+
+label1 =tk.Label(win,text="Successfully installed.",fg="#79a8a9",bg='#303841')
+label1.place(x=50, y=50)
+tk.Label(win,text="Chart",width=30,height=17,fg="#79a8a9",bg='#232931').place(x=480, y=90)
+
+win.mainloop()
